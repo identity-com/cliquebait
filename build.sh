495e2d490ac43c6b8a8d98264b72a8075dd20745
Vsevolods Mihailovs <cheelahim@gmail.com>
2018-10-26 16:17:54 +0300

    Adjust network params for marketplace testing and import default accounts

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 set -e
 set -x 
 
-export IMAGE_NAME=${IMAGE_NAME:-"foamspace/cliquebait"}
-export GETH_VERSION=${GETH_VERSION:-"v1.8.8"}
+export IMAGE_NAME=${IMAGE_NAME:-"civicteam/geth-privatenet-poa"}
+export GETH_VERSION=${GETH_VERSION:-"v1.8.17"}
 
 docker build --pull --build-arg GETH_VERSION=$GETH_VERSION -t $IMAGE_NAME:$GETH_VERSION .
-#docker tag $IMAGE_NAME:$GETH_VERSION $IMAGE_NAME:latest
+docker tag $IMAGE_NAME:$GETH_VERSION $IMAGE_NAME:stable
 
 # docker push $IMAGE_NAME:latest
 # docker push $IMAGE_NAME:$GETH_VERSION
